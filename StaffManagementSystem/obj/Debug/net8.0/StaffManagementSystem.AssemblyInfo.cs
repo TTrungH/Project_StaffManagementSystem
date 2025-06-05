b2b8477e1e1793be02b74c1215fb93c0cba8a9c4
HNDThuan <thuan.huynhnguyen.cit20@eiu.edu.vn>
2025-06-05 14:32:16 +0700

    init

--- StaffManagementSystem/obj/Debug/net8.0/StaffManagementSystem.AssemblyInfo.cs
+++ StaffManagementSystem/obj/Debug/net8.0/StaffManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StaffManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e4292f22f467988105b4e6c672c9b3a85ab7cab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1a2734161c504530816e0f9666e632c530ba3d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("StaffManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StaffManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
